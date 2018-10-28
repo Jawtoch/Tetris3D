6b09716df1a36eda3098983b4a4c4286b913445b
Ugo Cottin <ugo.cottin@gmail.com>
2018-10-28 18:28:10 +0100

    after restructuration cube

---
+++ Tetris3D/Classes/cube.hpp
@@ -0,0 +1,35 @@
+//
+//  cube.hpp
+//  Tetris3D
+//
+//  Created by ugo cottin on 25/10/2018.
+//  Copyright © 2018 ugocottin. All rights reserved.
+//
+
+#ifndef cube_hpp
+#define cube_hpp
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include "point2D.hpp"
+#include "square.hpp"
+
+#include "point3D.hpp" //Get ANGLE constant
+
+class Cube {
+    
+private:
+    double taille;
+    bool exist;
+    
+public:
+    Cube();
+    Cube(double taille);
+    void RenderDrawCube(SDL_Renderer* renderer, int a, int b, int c, int shiftX, int shiftY);
+    bool doesExist();
+};
+
+Cube*** createArray(int taille1, int taille2, int taille3);
+
+void freeArray(Cube*** cTab);
+#endif /* cube_hpp */
